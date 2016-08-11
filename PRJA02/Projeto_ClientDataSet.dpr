11b738a57478c095c4016cc64ec138433bcdf677
Felipe <felipe_14_oliveira@hotmail.com>
2016-08-10 22:24:39 -0300

    ok

---
+++ PRJA02/Projeto_ClientDataSet.dpr
@@ -0,0 +1,14 @@
+program Projeto_ClientDataSet;
+
+uses
+  Vcl.Forms,
+  Principal in 'Principal.pas' {Form1};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
